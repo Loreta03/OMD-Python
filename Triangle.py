ac327036cdfb442a87621fddfc34c930ebb695c8
Loreta03 <37341706+Loreta03@users.noreply.github.com>
2022-02-28 21:29:25 +0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -41,3 +41,12 @@ Paul = Lunch("menu 1")
 Paul.menu_price()
 
 #Problem 4
+class Point3D():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.x, self.y, self.z)
+my_point = Point3D(1, 2, 3)
+print(my_point)
